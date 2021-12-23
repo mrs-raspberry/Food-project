c1ae985ca0a5ab59d86528b549f739b1abc8ccb1
Anna <malinnikova-anna@mail.ru>
2021-12-23 19:50:40 +0300

    calory calculator, add functionality with localstorage and regexp

--- js/script.js
+++ js/script.js
@@ -402,9 +402,36 @@ window.addEventListener('DOMContentLoaded', () => {
     // Calculator
     const result = document.querySelector(".calculating__result span");
 
-    let sex = "female",
-        height, weight, age,
+    let sex, height, weight, age, ratio;
+
+    if (localStorage.getItem("sex")){//проверяем есть ли данные по 1 и 3 блокам в локал сторадж, если да, то подставляем их в калькулятор, если нет - дефолтные и сохраняем их в локалсторадж
+        sex = localStorage.getItem("sex")
+    }else{
+        sex = "female";
+        localStorage.setItem("sex", "female");
+    };
+    if (localStorage.getItem("ratio")){
+        ratio = localStorage.getItem("ratio")
+    }else{
         ratio = 1.375;
+        localStorage.setItem("ratio", "1.375");
+    };
+    
+    function setActiveClass (selector, activeClass){//устанавливаем класс активности на ту кнопку, кот сохранена в локально
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem =>{
+            elem.classList.remove(activeClass);
+            if (elem.getAttribute("id") === localStorage.getItem("sex")){
+                elem.classList.add(activeClass);
+            }
+            if (elem.getAttribute("data-ratio") === localStorage.getItem("ratio")){
+                elem.classList.add(activeClass);
+            }
+        })
+    }
+    setActiveClass("#gender div", "calculating__choose-item_active");//вызываем Ф для дивов из каждого блока отдельно
+    setActiveClass(".calculating__choose_big div", "calculating__choose-item_active");
     
     function calcTotal () {
         if(!sex || !height || !weight || !age || !ratio){//проверяем все ли поля заполнены данными
@@ -420,15 +447,17 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     calcTotal();
 
-    function getStaticInfo (parentSeletor, activeClass){
-        const elements = document.querySelectorAll(`${parentSeletor} div`);
+    function getStaticInfo (seletor, activeClass){
+        const elements = document.querySelectorAll(`${seletor}`);
 
         elements.forEach((elem) => {
             elem.addEventListener("click", (event) => {
                 if (event.target.getAttribute("data-ratio")){//если кнопка, на кот кликнули содержит дата аттрибут, то это последний блок, берем себе значение этого аттрибута
-                    ratio = event.target.getAttribute("data-ratio");
+                    ratio = +event.target.getAttribute("data-ratio");
+                    localStorage.setItem("ratio", +event.target.getAttribute("data-ratio"));
                 }else{//если нет, то это первый блок и мы берем пол
-                    sex = event.target.getAttribute("id");
+                    sex = +event.target.getAttribute("id");
+                    localStorage.setItem("sex", +event.target.getAttribute("id"));
                 };
 
                 elements.forEach(item => {//меняем при клике класс активности
@@ -440,13 +469,19 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
-    getStaticInfo("#gender", "calculating__choose-item_active");//вызываем Ф для каждого блока отдельно, класс активности одинаковый
-    getStaticInfo(".calculating__choose_big", "calculating__choose-item_active")
+    getStaticInfo("#gender div", "calculating__choose-item_active");//вызываем Ф для дивов из каждого блока отдельно, класс активности одинаковый
+    getStaticInfo(".calculating__choose_big div", "calculating__choose-item_active")
 
     function getDynamicInfo (field) {//Ф для каждого поля ввода
         const inputs = document.querySelectorAll(field);
         inputs.forEach(input => {
             input.addEventListener("input", () => {
+                if (input.value.match(/\D/g)){//добавляем проверку, чтобы подсветить поле, где неверно введена информация, т.е содержатся не цифры!
+                    input.style.border = "1px solid red";
+                }else{
+                    input.style.border = "none";
+                };
+
                switch(input.getAttribute("id")) {//проверяем значение id, в каждом кейсе присваиваем значение своей переменную
                     case "height": 
                     height = +input.value;
